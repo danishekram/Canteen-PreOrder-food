c71e64068fde4e3339c35f3157b8624197eb5c5c
Danish Ekram <danishekram188@gmail.com>
2026-08-27 18:53:53 +0530

    Update code files

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'tiffin-cache-v2';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Forces new SW to activate immediately
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim()) // Takes control of all open pages immediately
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener('fetch', (event) => {
+  // Network first, falling back to cache
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        return networkResponse;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
